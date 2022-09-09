7c1d824ca26567fab003a0d655e03e2b9dcd2733
GraceTatiana <112901608+GraceTatiana@users.noreply.github.com>
2022-09-09 08:40:10 -0500

    Update java.js

--- introJS/java.js
+++ introJS/java.js
@@ -1,3 +1,9 @@
+function myFunctionTest(expected, found) {
+    let predicate;
+    if (Array.isArray(found)) predicate = arraysAreEqual(expected, found);
+    else predicate = expected === found;
+    return predicate ? "TEST SUCCEEDED" : "TEST FAILED. Expected " + expected + " found " + found;
+}
 function max(x,y){
     if(x > y){
         return x;
@@ -5,7 +11,7 @@ function max(x,y){
         return y;
     }
 }
-console.log("the expected output of max(10,20) is 20 and mine is:" + max(10,20) )
+console.log("Expected output of max(20,10) is 20 -> " + myFunctionTest(20, max(20, 10)));
 
 function maxOfThree(x, y, z){
     return max(max(x,y), z);
@@ -18,7 +24,7 @@ function maxOfThree(x, y, z){
     // }
     
 }
-console.log("the expected output of maxOfThree(10,20,30) is 30 and mine is:" + maxOfThree(10,20,30) )
+console.log("Expected output of maxOfThree(55,4,44) is 55 -> " + myFunctionTest(55, maxOfThree(55, 4, 44)));
 function isVowel(x){
 
     // if (x === 'u'){
@@ -39,8 +45,8 @@ function isVowel(x){
             return false;
     }
 }
-console.log("Expected output of isVowel('a') is true -> " + isVowel('a'));
-console.log("Expected output of isVowel('c') is false -> " + isVowel('c'));
+console.log("Expected output of isVowel('a') is true -> " + myFunctionTest(true, isVowel("a")));
+console.log("Expected output of isVowel('a') is true -> " + myFunctionTest(true, isVowel("a")));
 function sum(x){
     var sum = 0;
     for(let i = 0; i < x.length; i++ ){
@@ -49,7 +55,7 @@ function sum(x){
     }
     return sum;
 }
-console.log("Expected output of sum([100,10,5.5]) is 115.5 -> " + sum([100,10,5.5]));
+console.log("Expected output of sum([1,2,3,4]) is 10 -> " + myFunctionTest(10, sum([1, 2, 3, 4])));
 
 function multiply(x){
     var mul = 1;
@@ -59,8 +65,7 @@ function multiply(x){
     }
     return mul;
 }
-console.log("Expected output of sum([100,10,5.5]) is 5500 -> " + multiply([100,10,5.5]));
-
+console.log("Expected output of multiply([1,2,3,4]) is 10 -> " + myFunctionTest(24, multiply([1, 2, 3, 4])));
 function reverse(y){
     var rev = " ";
     for(let i= y.length - 1; i >= 0 ;i-- ) {
@@ -69,27 +74,35 @@ function reverse(y){
     return rev;
 
 }
-console.log("Expected output of reverse(\"Hello\") is  -> " + reverse("Hello"));
+console.log("Expected output of reverse('xylem') is melyx -> " + myFunctionTest("melyx", reverse("xylem")));
 
-// function findLongestWord(z){
-//     var word = " ";
-//     for(let i = 0; i < z.length ;i++ ) {
-//        if(z[i].length > z[i + 1].length){
-//             word += z[i];
-//        }
-//     }
-//     return word;
-// }
 function findLongestWord(words) {
     return words.reduce((longestLength, word) => {
         return longestLength > word.length ? longestLength : word.length;
     }, 0);
 }
-console.log("Expected output of findLongestWord([\"Bat\", \"Batman\", \"BatMobile\", \"Robin\"]) is  -> " + findLongestWord(["Bat", "Batman","BatMobile", "Robin"]));
+console.log("Expected output of findLongestWord(['One','Three','Configure','xylem']) is 9 => " +
+    myFunctionTest(9, findLongestWord(['One', 'Three', 'Configure', 'xylem'])));
 
-function filterLongWords(words, i) {
-    return words.filter(word => word.length > i);
-}
-
-console.log("Expected output of filterLongWords(['One','Threes','Configure','xylem'], 5) is ['Threes','Configure'] => " +
- filterLongWords(["One", "Threes", "Configure", "xylem"], 5));
+    function filterLongWords(words, i) {
+        return words.filter(word => word.length > i);
+    }
+    
+    console.log("Expected output of filterLongWords(['One','Threes','Configure','xylem'], 5) is ['Threes','Configure'] => " +
+        myFunctionTest(["Threes", "Configure"], filterLongWords(["One", "Threes", "Configure", "xylem"], 5)));
+    
+    
+    
+    
+    const a = [1, 3, 5, 3, 3];
+    console.log(`Initial Array a: ${a}`);
+    
+    const multiplyBy10 = a.map(element => element * 10);
+    console.log(`All elements multiplied by 10 => ${multiplyBy10} ${myFunctionTest([10, 30, 50, 30, 30], multiplyBy10)}`);
+    
+    
+    const threeArray = a.map(() => 3);
+    console.log(`Array with all elements equal to 3 => ${threeArray} ${myFunctionTest([3, 3, 3, 3, 3], threeArray)}`);
+    
+   
+    console.log(`Product of all elements => ${multiply(a)} ${myFunctionTest(135, multiply(a))}`)
