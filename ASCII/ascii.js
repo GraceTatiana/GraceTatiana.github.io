3776ee5dac767136579cd5cd4e401c5ae907fdbe
GraceTatiana <112901608+GraceTatiana@users.noreply.github.com>
2022-09-12 08:35:55 -0500

    Update ascii.js

--- ASCII/ascii.js
+++ ASCII/ascii.js
@@ -1,65 +1,60 @@
-"use strict";
-window.onload = function(){
-    
+window.onload = function () {
+    "use strict";
+   
+    let animationIntervalId;
+    const DEFAULT_ANIMATION_INTERVAL_MS = 250;
+    const TURBO_ANIMATION_INTERVAL_MS = 50;
 
-    let animationIntervalID;
-    const DEFAULT_ANIMATION_INTERVAL = 250;
-    const TURBO_ANIMATION_INTERVAL = 50;
-/* event listeners */
-document.getElementById("start").addEventListener("click", startAnimation )
-document.getElementById("stop").addEventListener("click", stopAnimation )
-document.getElementById("animation").addEventListener("change", displayAnimation)
-document.getElementById("fontsize").addEventListener("change", changeFont)
-document.getElementById("turbo").addEventListener("change", function (){
-    if(document.getElementById("start").hasAttribute("disabled")){
-        clearInterval(animationIntervalID);
+    /* ------ Event listeners ------ */
+    document.getElementById("animation").addEventListener("change", function () {
         const animation = document.getElementById("animation").value;
-        const timeout = this.checked ? TURBO_ANIMATION_INTERVAL : DEFAULT_ANIMATION_INTERVAL;
-        animationIntervalID = createAnimationInterval(animation, timeout);
+        document.getElementById("text-area").value = ANIMATIONS[animation];
+    });
+    document.getElementById("fontsize").addEventListener("change", function () {
+        document.getElementById("text-area").style.fontSize =
+            document.getElementById("fontsize").value;
+    });
+    document.getElementById("start").addEventListener("click", function () {
+       
+        document.getElementById("start").disabled = true;
+        document.getElementById("stop").disabled = false;
+        document.getElementById("animation").disabled = true;
+        clearInterval(animationIntervalId);
+
+        const animation = document.getElementById("animation").value;
+        const isTurbo = document.getElementById("turbo").checked;
+        const timeout = isTurbo ? TURBO_ANIMATION_INTERVAL_MS : DEFAULT_ANIMATION_INTERVAL_MS;
+        animationIntervalId = createAnimationInterval(animation, timeout);
     }
-});
-
-
-/* functions */
-function startAnimation(){
-    document.getElementById("start").disabled = true;
-    document.getElementById("stop").disabled = false;
-    document.getElementById("animation").disabled = true;
-    clearInterval(animationIntervalID);
-
-    const animation = document.getElementById("animation").value;
-    const isTurbo = document.getElementById("turbo").checked;
-    const timeout = isTurbo ? TURBO_ANIMATION_INTERVAL : DEFAULT_ANIMATION_INTERVAL;
-    animationIntervalID = createAnimationInterval(animation, timeout);
-}
-
-function stopAnimation(){
-    document.getElementById("start").disabled = false;
-    document.getElementById("stop").disabled = true;
-    document.getElementById("animation").disabled = false;
-    clearInterval(animationIntervalID);
-}
-
-function displayAnimation(){
-    const animation = document.getElementById("animation").value;
-    document.getElementById("text-area").value = ANIMATIONS[animation];
-}
-
-function changeFont(){
-    document.getElementById("text-area").style.fontSize = document.getElementById("fontsize").value;
-}
-
-const createAnimationInterval = (animation, timeout) =>{
-    const frames = ANIMATIONS[animation].split("=====");
-    let loopInterval = -1;
-
-    return setInterval(function (){
-        ++loopInterval;
-        if(loopInterval >= frames.length){
-            loopInterval = 0;
+);
+    document.getElementById("stop").addEventListener("click", function () {
+       
+        document.getElementById("stop").disabled = true;
+        document.getElementById("start").disabled = false;
+        document.getElementById("animation").disabled = false;
+        clearInterval(animationIntervalId);
+    });
+    document.getElementById("turbo").addEventListener("change", function () {
+        
+        if (document.getElementById("start").hasAttribute("disabled")) {
+            clearInterval(animationIntervalId);
+            const animation = document.getElementById("animation").value;
+            const timeout = this.checked ? TURBO_ANIMATION_INTERVAL_MS : DEFAULT_ANIMATION_INTERVAL_MS;
+            animationIntervalId = createAnimationInterval(animation, timeout);
         }
-        document.getElementById("text-area").value = frames[loopInterval];
-    }, timeout);
-};
-};
+    });
+   
+    const createAnimationInterval = (animation, timeout) => {
+        const frames = ANIMATIONS[animation].split("=====");
+        let loopIndex = -1;
+        return setInterval(function () {
+            ++loopIndex;
+            if (loopIndex >= frames.length) {
+                loopIndex = 0;
+            }
+            document.getElementById("text-area").value = frames[loopIndex];
+        }, timeout);
+    };
 
+    
+};
