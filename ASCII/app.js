e0357a811ba32ba55c439ab049ca5d1aaeb60db1
GraceTatiana <112901608+GraceTatiana@users.noreply.github.com>
2022-09-12 08:35:50 -0500

    Delete app.js

--- ASCII/app.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-window.onload = function(){
-    document.getElementById("button").addEventListener("click", function (){
-       const inputName = document.getElementById("name").value ;
-       document.getElementById("result").value = inputName;
-       document.getElementById("name").value = "";
-    })
-}
